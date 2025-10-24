cc8d165b30abc9f71cac6aea6922e5f0887abbb1
crescentchronicle <aalefort@outlook.com>
2025-10-24 01:32:20 -0700

    added chara_count function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import character_count
 
 def main():
     #frankenstein_text = get_book_text("./books/frankenstein.txt")
@@ -6,6 +7,10 @@ def main():
 
     frankenstein_word_count = get_num_words("./books/frankenstein.txt")
     print(f"Found {frankenstein_word_count} total words")
+
+    frankenstein_chara_count = character_count("./books/frankenstein.txt")
+    print(frankenstein_chara_count)
+
 
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,16 @@ def get_num_words(path_to_file):
     split_words = file_contents.split()
     word_count = len(split_words)
     return word_count
+
+def character_count(path_to_file):
+    chara_dictionary: dict[str, int] = {}
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    file_contents = file_contents.lower()
+    file_contents = list(file_contents)
+    for chara in file_contents:
+        if chara not in chara_dictionary:
+            chara_dictionary[chara] = 1
+        else:
+            chara_dictionary[chara] += 1
+    return chara_dictionary
