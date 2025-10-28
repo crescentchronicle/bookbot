079c2c2fb3770d688ea24ba9b6740211a782d57a
crescentchronicle <aalefort@outlook.com>
2025-10-27 21:02:49 -0700

    added chara_sort, helper sort_on functions to stats.py, added report print-out to main.py utilizing chara_sort.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import character_count
+from stats import sort_counts
 
 def main():
     #frankenstein_text = get_book_text("./books/frankenstein.txt")
@@ -8,8 +9,20 @@ def main():
     frankenstein_word_count = get_num_words("./books/frankenstein.txt")
     print(f"Found {frankenstein_word_count} total words")
 
-    frankenstein_chara_count = character_count("./books/frankenstein.txt")
+    frankenstein_chara_count = sort_counts("./books/frankenstein.txt")
     print(frankenstein_chara_count)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {frankenstein_word_count} total words")
+    print("--------- Character Count -------")
+    for i in range(0, len(frankenstein_chara_count)):
+        if frankenstein_chara_count[i]["name"].isalpha() == True:
+            print(f"{frankenstein_chara_count[i]["name"]}: {frankenstein_chara_count[i]["num"]}")
+        else:
+            continue
+    print("============= END ===============")
 
 
 
--- stats.py
+++ stats.py
@@ -24,3 +24,36 @@ def character_count(path_to_file):
         else:
             chara_dictionary[chara] += 1
     return chara_dictionary
+
+def sort_on(items):
+    return items["num"]
+
+def sort_counts(path_to_file):
+    #create function-level variables
+    chara_counts_list = []
+    chara_counts_dict: dict[str, int] = {}
+    #open file and change to string
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    #change string to list of separated characters
+    file_contents = file_contents.lower()
+    file_contents = list(file_contents)
+    #make a loop to parse through characters and adds to chara_counts_dict
+    for chara in file_contents:
+        if chara not in chara_counts_dict:
+            chara_counts_dict[chara] = 1
+        else:
+            chara_counts_dict[chara] += 1
+    #make loop that reads chara_counts_dict and splits key and value pairs into more granular, 
+    # smaller dictionaries, with two keys: "name" and "num"
+    chara_counts_dict_keys = list(chara_counts_dict.keys())
+    for i in range (0, len(chara_counts_dict_keys)):
+        chara_data = {}
+        chara_data.clear()
+        chara_data["name"] = chara_counts_dict_keys[i]
+        chara_data["num"] = chara_counts_dict[chara_counts_dict_keys[i]]
+        #add smaller dictionaries to chara_counts list
+        chara_counts_list.append(chara_data)
+    #sort list of dictionaries from greatest to least
+    chara_counts_list.sort(reverse=True, key=sort_on)
+    return chara_counts_list
