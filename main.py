0579362bb2dce12f2cceaf1843dd5d1d9e85e299
crescentchronicle <aalefort@outlook.com>
2025-10-27 21:30:33 -0700

    changed main.py to accept sysarg[1] as input to allow for analysis of any book in books/

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
+import sys
 from stats import get_num_words
-from stats import character_count
 from stats import sort_counts
 
 def main():
-    #frankenstein_text = get_book_text("./books/frankenstein.txt")
-    #print(frankenstein_text)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)     
+    
+    word_count = get_num_words(sys.argv[1])
 
-    frankenstein_word_count = get_num_words("./books/frankenstein.txt")
-    print(f"Found {frankenstein_word_count} total words")
-
-    frankenstein_chara_count = sort_counts("./books/frankenstein.txt")
-    print(frankenstein_chara_count)
+    chara_count = sort_counts(sys.argv[1])
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print("Analyzing book found at BOOK...")
     print("----------- Word Count ----------")
-    print(f"Found {frankenstein_word_count} total words")
+    print(f"Found {word_count} total words")
     print("--------- Character Count -------")
-    for i in range(0, len(frankenstein_chara_count)):
-        if frankenstein_chara_count[i]["name"].isalpha() == True:
-            print(f"{frankenstein_chara_count[i]["name"]}: {frankenstein_chara_count[i]["num"]}")
+    for i in range(0, len(chara_count)):
+        if chara_count[i]["name"].isalpha() == True:
+            print(f"{chara_count[i]["name"]}: {chara_count[i]["num"]}")
         else:
             continue
     print("============= END ===============")
+    #print(sys.argv)
 
 
 
